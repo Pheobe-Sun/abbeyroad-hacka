5b7e6115261f3c3072c10cea3ba2962e39bb48d5
Pheobe <Pheobe.sun@gmail.com>
2019-11-09 21:39:55 +0000

    Audio Playback

--- Play_audio.py
+++ Play_audio.py
@@ -1,2 +1,13 @@
-from playsound import playsound
-playsound('test_voice.wav')
+from pydub import AudioSegment
+from pydub.playback import play
+
+
+sound_load_1 = "test_voice.wav"
+sound_play_1 = AudioSegment.from_wav(sound_load_1)
+
+# convert audio
+# awesome.export(target_loc, format="mp3")
+
+play(sound_play_1)
+
+
